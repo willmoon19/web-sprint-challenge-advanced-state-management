6964ef109b1504a73c6e3ab9ed4501c2fd843693
William Moon <william-moon@lambdastudents.com>
2021-05-14 09:29:13 -0700

    index set up

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 
 import "./index.css";
 import App from "./App";
+import reducer from "./reducers/index"
 
 const { worker } = require('./mocks/browser');
 worker.start();
 
 const rootElement = document.getElementById("root");
 
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App />
+    </Provider>, 
     rootElement
 );
 
